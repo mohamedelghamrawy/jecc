e6659efc3bf37b3758abfa167dc8b21bf57e6ce3
Mohamed ElGhamrawy <mhelghamrawy@gmail.com>
2021-11-16 00:09:10 +0100

    Revert "Testing EcPoint"
    This reverts commit 0c40768a167e398aabe7fa66378331eee6201080.

--- src/com/github/mhelghamrawy/ie7cryptographyecc/Application.java
+++ src/com/github/mhelghamrawy/ie7cryptographyecc/Application.java
@@ -5,9 +5,6 @@ import java.math.BigInteger;
 public class Application {
 
     public static void main(String[] args) {
-        EcPoint point1 = new EcPoint(new BigInteger("1"), new BigInteger("2"));
-        EcPoint point2 = new EcPoint(new BigInteger("1"), new BigInteger("3"));
 
-        System.out.println(point1.equals(point2));
     }
 }
