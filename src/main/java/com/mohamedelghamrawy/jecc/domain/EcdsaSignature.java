158b87e9c52043fd10c22422c97a13013e678dae
Mohamed ElGhamrawy <mhelghamrawy@gmail.com>
2022-04-11 12:36:07 +0200

    ECDSA partially working

---
+++ src/main/java/com/mohamedelghamrawy/jecc/domain/EcdsaSignature.java
@@ -0,0 +1,10 @@
+/**
+EcdsaSignature class implements
+
+@author  mhelghamrawy
+@version 1.0
+@since 10.04.22      
+*/
+
+package com.mohamedelghamrawy.jecc.domain;public class EcdsaSignature {
+}
---
+++ src/main/java/com/mohamedelghamrawy/jecc/util/Ecdsa.java
@@ -0,0 +1,10 @@
+/**
+Ecdsa class implements
+
+@author  mhelghamrawy
+@version 1.0
+@since 10.04.22      
+*/
+
+package com.mohamedelghamrawy.jecc.util;public class Ecdsa {
+}
